54e7f1095adb5eb9ddc62d823b6e6590db4d393f
stian96 <stiankru@hiof.no>
2023-05-13 21:26:02 +0200

    added a normal print_tree method and an internal recursive print_tree method.

--- datastructures/trees/binarytree.py
+++ datastructures/trees/binarytree.py
@@ -2,11 +2,16 @@ from node import Node
 
 
 class BinIntTree:
+    """
+    A binary tree class containing integer values.
+    """
 
     def __init__(self):
         self.root = None
 
     def insert(self, value):
+
+        # Is root empty?
         if self.root is None:
             self.root = Node(value)
         else:
@@ -14,6 +19,7 @@ class BinIntTree:
             parent = None
             left_child = False
 
+            # Loop until we find an available space.
             while current is not None:
                 parent = current
 
@@ -29,10 +35,24 @@ class BinIntTree:
                     current = current.right
                     left_child = False
 
+            # Insert new node in left or right child.
             if left_child:
                 parent.left = Node(value)
             else:
                 parent.right = Node(value)
+
+    def print_tree(self):
+        if self.root is not None:
+            self._print_tree(self.root)
+
+        return Exception("Root is empty, cannot print tree.")
+
+    def _print_tree(self, node):
+        if node is not None:
+            self._print_tree(node.left)
+            print(str(node.value))
+            self._print_tree(node.right)
+
 
 
 
