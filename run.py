b8dd1b0088c43b2e02e76fefd72f3e122e73deb8
stian96 <stiankru@hiof.no>
2023-05-19 14:10:55 +0200

    tested heapsort.

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
+from app.algorithms.sorting_algorithms import SortingAlgorithms
+
+array = [4, 100, 1, 56, 23, 7, 8, 5, 99]
+sort = SortingAlgorithms(array)
+
+# Perform heap sort
+print(sort.heap_sort())
+
 
