7104fac989f85511a71536db069abecb5858f64b
stian96 <stiankru@hiof.no>
2023-05-30 22:29:53 +0200

    wrote docs and init method.

---
+++ app/algorithms/searching_algorithms.py
@@ -0,0 +1,9 @@
+class SearchingAlgorithms:
+    """
+    Class that contains functions for the following search algorithms:
+    1. Linear search
+    2. Binary search
+    """
+    def __init__(self, array):
+        self.array = array
+
