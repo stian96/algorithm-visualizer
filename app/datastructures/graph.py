5b2136ff2f5132225fbab9999fffb7fb1dcef6af
stian96 <stiankru@hiof.no>
2023-05-31 22:14:37 +0200

    implemented function to add edges to a graph, and to display the nodes in the graph.

--- app/datastructures/graph.py
+++ app/datastructures/graph.py
@@ -5,4 +5,22 @@ class Graph:
     a graph as a dictionary, where keys are the nodes
     in the graph, and values are lists of nodes that
     are directly connected to the key node.
+
+    Note: This implementation is for directed graphs.
     """
+    def __init__(self):
+        self.graph = dict()
+
+    def add_edge(self, node, neighbour):
+        if node not in self.graph:
+            self.graph[node] = neighbour
+        else:
+            self.graph[node].append(neighbour)
+
+    def display(self):
+        for node in self.graph:
+            for neighbour in self.graph[node]:
+                print("(", node, ", ", neighbour, ")")
+
+
+
