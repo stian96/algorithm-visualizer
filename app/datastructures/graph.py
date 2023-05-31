5039a154c8496b3752c5340d3afd4ac29d5be3cf
stian96 <stiankru@hiof.no>
2023-05-31 22:34:53 +0200

    wrote docs for some functions.

--- app/datastructures/graph.py
+++ app/datastructures/graph.py
@@ -8,6 +8,7 @@ class Graph:
 
     Note: This implementation is for directed graphs.
     """
+
     def __init__(self):
         self.graph = dict()
 
@@ -22,7 +23,13 @@ class Graph:
             self.graph[node].remove(neighbour)
 
     def remove_node(self, node):
+        """
+        Function that removes a specific node from the graph.
+        It also removes the neighbour relationship with nodes that
+        has the node to be removed as a neighbour.
 
+        :param node: The node to be removed.
+        """
         # Delete the node.
         if node in self.graph:
             del self.graph[node]
@@ -33,16 +40,26 @@ class Graph:
             self.graph[other_node] = [n for n in self.graph[other_node] if n != node]
 
     def get_nodes(self):
+        """
+        Function that returns a list of all the nodes in the graph.
+
+        :return: A list of all the nodes in the graph.
+        """
         return list(self.graph.keys())
 
     def get_neighbours(self, node):
+        """
+        Function that returns all the neighbours of a specific node if
+        it exists. The default value is a empty list.
+        :param node: The node to get the neighbours for.
+        :return: A list of all neighbours.
+        """
         return self.graph.get(node, [])
 
-
     def display(self):
+        """
+        Function that display the graph in (node, neighbour) pairs.
+        """
         for node in self.graph:
             for neighbour in self.graph[node]:
                 print("(", node, ", ", neighbour, ")")
-
-
-
