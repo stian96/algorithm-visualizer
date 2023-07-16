6b61b6b1d42c4bfc5bc0e8183db33d6338c5f4bd
stian96 <stiankru@hiof.no>
2023-07-17 00:53:23 +0200

    worked on frontend

--- app/static/js/binary_tree.js
+++ app/static/js/binary_tree.js
@@ -1,119 +1,120 @@
-let levelCounter = 1;
-var root = null;
-let currentNode = null;
-
-// When page has loaded.
-window.onload = function() {
-    var button = document.getElementById("insert-btn");
-
-    // Add event listener for click
-    button.addEventListener('click', function() {
-        var value = document.getElementById('value-input').value;
-        var binary_tree = document.getElementsByClassName('level-' + levelCounter)[0];
-
-        // Send data to Flask endpoint '/insert'.
-        var xml_request = new XMLHttpRequest();
-        xml_request.open('POST', '/insert', true);
-        xml_request.setRequestHeader('Content-type', 'application/json');
-
-        xml_request.onreadystatechange = function() {
-            if (xml_request.readyState === XMLHttpRequest.DONE) {
-                if (xml_request.status === 200) {
-                    var response = JSON.parse(xml_request.responseText);
-                    console.log(response.message);
-
-                    // Update tree in frontend and redraw nodes.
-                    insertNode(response['node-value']);
-                    currentNode = findNode(response['node-value'], root);  // keep track of the current node
-                    redrawNodes(binary_tree);
-                } else {
-                    console.error('Error:', xml_request.responseText);
-                }
-            }
-        };
-        xml_request.send(JSON.stringify({ 'node-value': value }));
-        levelCounter++;
-    });
-
-    function insertNode(nodeValue) {
-        if (root === null) {
-            root = createNode(nodeValue);
+class Node {
+    constructor(value, depth, parent = null) {
+      this.value = value;
+      this.depth = depth;
+      this.left = null;
+      this.right = null;
+      this.parent = parent;
+    }
+  }
+  
+  class BinaryTree {
+    constructor() {
+      this.root = null;
+    }
+  
+    insert(value) {
+      let newNode = new Node(value, 0);
+  
+      if (this.root === null) {
+        this.root = newNode;
+      } 
+      else {
+        this._insertNode(this.root, newNode, 1);
+      }
+    }
+  
+    _insertNode(node, newNode, depth) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
+            newNode.depth = depth;
+            newNode.parent = node;
+            node.left = newNode;
         } 
         else {
-            var currentNode = root;
-            while (true) {
-                if (nodeValue < currentNode.value) {
-                    if (currentNode.left === null) {
-                        currentNode.left = createNode(nodeValue);
-                        break;
-                    } 
-                    else {
-                        currentNode = currentNode.left;
-                    }
-                } 
-                else if (nodeValue > currentNode.value) {
-                    if (currentNode.right === null) {
-                        currentNode.right = createNode(nodeValue);
-                        break;
-                    } 
-                    else {
-                        currentNode = currentNode.right;
-                    }
-                } 
-                else {
-                    // Node with the same value already exists.
-                    break;
-                }
-            }
+            this._insertNode(node.left, newNode, depth + 1);
         }
-    }
-
-    function createNode(nodeValue) {
-        return {
-            value: nodeValue,
-            left: null,
-            right: null
-        };
-    }
-
-    function redrawNodes(treeArea) {
-        // Clear the binary tree area.
-        treeArea.innerHTML = "";
-    
-        // Recursive helper function to create and append nodes.
-        function createNodeElement(nodeData, parentElement, level) {
-            if (nodeData === null) {
-                return;
-            }
-    
-            // Create a new div for each node
-            var nodeElement = document.createElement('div');
-            nodeElement.className = 'node fade-in level-' + level;
-            nodeElement.innerHTML = nodeData.value;
-            parentElement.appendChild(nodeElement);
-    
-            // Recursively create and append child nodes
-            if (nodeData.left !== null) {
-                createNodeElement(nodeData.left, nodeElement, level + 1);
-            }
-            if (nodeData.right !== null) {
-                createNodeElement(nodeData.right, nodeElement, level + 1);
-            }
+      } 
+      else {
+        if (node.right === null) {
+            newNode.depth = depth;
+            newNode.parent = node;
+            node.right = newNode;
+        } 
+        else {
+          this._insertNode(node.right, newNode, depth + 1);
         }
-    
-        // Call the helper function with the root node of the tree.
-        createNodeElement(root, treeArea, 1);
+      }
     }
-
-    function findNode(value, node) {
-        if (node === null) {
-            return null;
-        }
-        if (node.value === value) {
-            return node;
-        }
-
-        return findNode(value, node.left) || findNode(value, node.right);
+  
+    draw(container) {
+      // Clear the container first.
+      container.innerHTML = '';
+      this._drawNodes(this.root, container);
     }
+  
+    _drawNodes(node, container, parentLeft = 0, parentRight = 0) {
+        if (node !== null) {
+          let div = document.createElement('div');
+          div.className = 'node';
+          div.textContent = node.value;
+
+          var left = 200;
+          var right = 200;
+      
+          if (node.depth != 0) {
+            if (node.depth <= 2) {
+                div.style.top = `${80}px`;
+            }
+            else if (node.depth <= 6) {
+                div.style.top = `${160}px`;
+            }
+            
+
+            if (node.value < node.parent.value) {
+              div.style.left = `${left}px`;
+              left += 200;
+            } 
+            else {
+              div.style.right = `${right}px`;
+              right += 200;
+            }
+          }
+          else {
+            div.style.left = '50%';
+          } 
+          
+          container.appendChild(div);
+      
+          this._drawNodes(node.left, container, parentLeft, parentRight);
+          this._drawNodes(node.right, container, parentLeft, parentRight);
+        }
+      }
+  }
+
+  window.onload = function() {
+    // Create a new binary tree
+    var tree = new BinaryTree();
+
+    // Get references to the form input and the tree container
+    var input = document.getElementById('value-input');
+    var container = document.getElementById('tree-container');
+
+    // Add event listener for click on the "Insert" button
+    document.getElementById('insert-btn').addEventListener('click', function() {
+        // Get the value from the input
+        var value = input.value;
+        console.log(value);
+
+        // Insert the new value into the tree
+        tree.insert(value);
+
+        // Draw the tree
+        tree.draw(container);
+
+        // Clear the input
+        input.value = '';
+    });
 };
 
+  
