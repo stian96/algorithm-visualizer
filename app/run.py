40384a94910aba6a7469889807d49b0a8c8bcebc
stian96 <stiankru@hiof.no>
2023-07-16 15:01:20 +0200

    created new endpoint in flask app

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from datastructures.binarytree import BinaryTree
 
 app = Flask(__name__)
@@ -9,6 +9,17 @@ tree = BinaryTree()
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/bintree')
+def bin_tree():
+    return render_template('binary_tree.html')
+
+@app.route('/insert', methods=['POST'])
+def insert_node():
+    value = request.form.get('node-value')
+    tree.insert(value)
+
+    return jsonify({'message': 'Node inserted successfully'})
 
 if __name__ == "__main__":
     app.run()
