fc216b81ee15182dc3e94fd4c88ec1b4cdf76119
stian96 <stiankru@hiof.no>
2023-07-19 17:08:43 +0200

    added js function to change color and reset color.

--- app/static/js/binary_tree.js
+++ app/static/js/binary_tree.js
@@ -34,6 +34,16 @@ let tree = new Node(100,
 // Function that changes color of a spesific node.
 function highlightNode(nodeValue, color = 'red') {
     let node = document.getElementById('node-' + nodeValue);
+    if (node) {
+        node.style.backgroundColor = color;
+    }
+}
 
+// Function to reset colors on all nodes.
+function resetColors() {
+    let nodes = document.getElementsByClassName('node');
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.backgroundColor = '#a5e99e';
+    }
 }
   
