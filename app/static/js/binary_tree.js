d243fb026dd055dbdd0c9170bdb860662caf48bd
stian96 <stiankru@hiof.no>
2023-07-19 17:00:06 +0200

    created the tree in js

--- app/static/js/binary_tree.js
+++ app/static/js/binary_tree.js
@@ -1,3 +1,4 @@
+// Class for the nodes in the binary tree.
 class Node {
     constructor(value, left = null, right = null) {
         this.value = value;
@@ -6,4 +7,27 @@ class Node {
     }
 }
 
+// The tree structure itself.
+let tree = new Node(100,
+    new Node(50,
+        new Node(45,
+            new Node(35),
+            new Node(47)
+        ),
+        new Node(63,
+            new Node(60),
+            new Node(77)
+        )
+    ),
+    new Node(120,
+        new Node(110,
+            new Node(102),
+            new Node(117)
+        ),
+        new Node(135,
+            new Node(125),
+            new Node(150)
+        )
+    )
+);
   
