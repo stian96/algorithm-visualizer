79c0476327d076815cd224122d0e7224a474b549
stian96 <stiankru@hiof.no>
2023-07-24 21:02:35 +0200

    worked on binary_tree.html

--- app/static/js/traversal.js
+++ app/static/js/traversal.js
@@ -84,19 +84,18 @@ addEventListenerForTraversal('level-order', '/levelorder');
 
 
 window.onload = function() {
-    document.addEventListener('DOMContentLoaded', function() {
-        let slider = document.getElementById("speed");
-        slider.oninput = function() {
-            let value = ((this.value - this.min) / (this.max - this.min)) * 100;
-            this.style.background = `linear-gradient(to right, #a5e99e ${value}%, #ffffff ${value}%)`;
-        }
+    let slider = document.getElementById("speed");
+    slider.oninput = function() {
+        let value = ((this.value - this.min) / (this.max - this.min)) * 100;
+        this.style.background = `linear-gradient(to right, #a5e99e ${value}%, #ffffff ${value}%)`;
+    }
     
-        // Trigger the input event manually for initial page load
-        slider.dispatchEvent(new Event('input'));
-    });
+    // Trigger the input event manually for initial page load
+    slider.dispatchEvent(new Event('input'));
 };
 
 window.onunload = function(){};
+
 
 
 
