ccc932c5b539147125dc8b83da57519cceb49124
stian96 <stiankru@hiof.no>
2023-07-24 18:12:47 +0200

    added the rest of the methods for traversal.

--- app/run.py
+++ app/run.py
@@ -19,6 +19,21 @@ def preorder_traversal():
     result = tree.preorder_traversal()
     return jsonify({'order': result})
 
+@app.route('/inorder', methods=['GET'])
+def inorder_traversal():
+    result = tree.inorder_traversal()
+    return jsonify({'order': result})
+
+@app.route('/postorder', methods=['GET'])
+def postorder_traversal():
+    result = tree.postorder_traversal()
+    return jsonify({'order': result})
+
+@app.route('/levelorder', methods=['GET'])
+def levelorder_traversal():
+    result = tree.level_order_traversal()
+    return jsonify({'order': result})
+
 if __name__ == "__main__":
     app.run()
 
