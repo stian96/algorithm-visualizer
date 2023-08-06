6b0ce8b7b11a8a3873e2519168a7cdd3566433db
stian96 <stiankru@hiof.no>
2023-08-06 16:00:28 +0200

    wrote test for binary search.

--- tests/test_searching_algorithms.py
+++ tests/test_searching_algorithms.py
@@ -41,3 +41,66 @@ class TestSearchingAlgorithms:
 
         # Assert.
         assert len(result) == 0
+
+    def test_binary_search_element_present(self):
+
+        # Setup
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        search = SearchingAlgorithms(array)
+
+        # Action
+        result = search.binary_search(7)
+
+        # Assert
+        assert result[-1]['value'] == 7
+        assert result[-1]['found'] == True
+
+    def test_binary_search_element_absent(self):
+
+        # Setup
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        search = SearchingAlgorithms(array)
+
+        # Action
+        result = search.binary_search(11)
+
+        # Assert
+        assert all([not step['found'] for step in result])
+
+    def test_binary_search_with_empty_array(self):
+
+        # Setup.
+        array = []
+        search = SearchingAlgorithms(array)
+
+        # Action.
+        result = search.binary_search(5)
+
+        # Assert.
+        assert len(result) == 0
+
+    def test_binary_search_element_at_start(self):
+
+        # Setup
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        search = SearchingAlgorithms(array)
+
+        # Action.
+        result = search.binary_search(1)
+
+        # Assert.
+        assert result[-1]['value'] == 1
+        assert result[-1]['found'] == True
+
+    def test_binary_search_element_at_end(self):
+
+        # Setup
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        search = SearchingAlgorithms(array)
+
+        # Action
+        result = search.binary_search(10)
+
+        # Assert.
+        assert result[-1]['value'] == 10
+        assert result[-1]['found'] == True
