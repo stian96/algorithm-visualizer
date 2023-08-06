5c9ce5212d6914119cb9ecb21c56138ca1bdc6c2
stian96 <stiankru@hiof.no>
2023-08-06 12:55:02 +0200

    .

--- app/static/js/sort.js
+++ app/static/js/sort.js
@@ -1,50 +1,55 @@
-// Set the initial slide index.
-let slideIndex = 0;
+(function() {
+    
+    // Set the initial slide index.
+    let slideIndex = 0;
 
-/**
- * Bind click event listener to a slider button.
- * @param {string} buttonId - The ID of the button ('next' or 'prev').
- */
-function bindSliderButton(buttonId) {
-    document.getElementById(buttonId).addEventListener('click', () => {
-        // Increment or decrement the slide index based on the button clicked.
-        slideIndex += (buttonId === 'next') ? 1 : -1;
-        updateSlider();
-    });
-}
+    /**
+     * Bind click event listener to a slider button.
+     * @param {string} buttonId - The ID of the button ('next' or 'prev').
+     */
+    function bindSliderButton(buttonId) {
+        document.getElementById(buttonId).addEventListener('click', () => {
+            // Increment or decrement the slide index based on the button clicked.
+            slideIndex += (buttonId === 'next') ? 1 : -1;
+            updateSlider();
+        });
+    }
 
-/**
- * Update the position of the slider based on the current slide index.
- */
-function updateSlider() {
-    const list = document.querySelector(".nav-list");
-    const items = document.querySelectorAll('.nav-items');
+    /**
+     * Update the position of the slider based on the current slide index.
+     */
+    function updateSlider() {
+        const list = document.querySelector(".nav-list");
+        const items = document.querySelectorAll('.nav-items');
 
-    // Ensure slide index is within bounds.
-    const maxSlideIndex = items.length - 3;
-    slideIndex = Math.max(0, Math.min(slideIndex, maxSlideIndex));
+        // Ensure slide index is within bounds.
+        const maxSlideIndex = items.length - 3;
+        slideIndex = Math.max(0, Math.min(slideIndex, maxSlideIndex));
 
-    // Calculate percentage to translate the slider.
-    const percentage = slideIndex * (100 / items.length);
-    list.style.transform = `translateX(-${percentage}%)`;
-}
+        // Calculate percentage to translate the slider.
+        const percentage = slideIndex * (100 / items.length);
+        list.style.transform = `translateX(-${percentage}%)`;
+    }
 
-/**
- * Bind click event listener to an element to open/close the info sidebar.
- * @param {string} elementId - The ID of the element to bind the click event to.
- */
-function toggleInfoSidebar(elementId) {
-    document.getElementById(elementId).addEventListener('click', () => {
-        const sidebar = document.getElementById('info-sidebar');
-        // Toggle sidebar width between 0 and 25rem.
-        sidebar.style.width = (sidebar.style.width === '0px') ? '25rem' : '0px';
-    });
-}
+    /**
+     * Bind click event listener to an element to open/close the info sidebar.
+     * @param {string} elementId - The ID of the element to bind the click event to.
+     */
+    function toggleInfoSidebar(elementId) {
+        document.getElementById(elementId).addEventListener('click', () => {
+            const sidebar = document.getElementById('info-sidebar');
+            // Toggle sidebar width between 0 and 25rem.
+            sidebar.style.width = (sidebar.style.width === '0px') ? '25rem' : '0px';
+        });
+    }
 
-// Bind the slider buttons.
-bindSliderButton('next');
-bindSliderButton('prev');
+    // Bind the slider buttons.
+    bindSliderButton('next');
+    bindSliderButton('prev');
 
-// Bind the open/close actions for the info sidebar.
-toggleInfoSidebar('help-icon');
-toggleInfoSidebar('close-icon');
+    // Bind the open/close actions for the info sidebar.
+    toggleInfoSidebar('help-icon');
+    toggleInfoSidebar('close-icon');
+
+});
+
