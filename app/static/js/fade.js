3ca5897ee4583b294651c133e7126c432b66d47e
stian96 <stiankru@hiof.no>
2023-08-26 17:07:53 +0200

    implemented animation for hamburger bars.

--- app/static/js/fade.js
+++ app/static/js/fade.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function toggleMenu() {
     const menu = document.getElementById("menu");
+    const hamburgerIcon = document.querySelector(".hamburger-menu");
     menu.classList.toggle("active");
+    hamburgerIcon.classList.toggle("active");
 }
 
 
