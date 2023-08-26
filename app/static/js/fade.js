ea98343ac9af44902a9a5af4b7301b3a4469ebe0
stian96 <stiankru@hiof.no>
2023-08-26 18:14:10 +0200

    improved nav menu

--- app/static/js/fade.js
+++ app/static/js/fade.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".header.fade-in").classList.add("active");
 });
 
+window.addEventListener("load", function() {
+    window.scrollTo(0, 0);
+});
+
 function toggleMenu() {
     const menu = document.getElementById("menu");
     const hamburgerIcon = document.querySelector(".hamburger-menu");
@@ -9,5 +13,22 @@ function toggleMenu() {
     hamburgerIcon.classList.toggle("active");
 }
 
+function adjustHeaderVisibility() {
+    const header = document.querySelector('.header');
+    let lastScrollTop = 0;
 
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop && scrollTop > 50) {
+            header.classList.add('hide');
+        } else {
+            header.classList.remove('hide');
+        }
+
+        lastScrollTop = scrollTop;
+    });
+}
+
+adjustHeaderVisibility();
 document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
