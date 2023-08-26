3b83e1961ad80b5c4771ae0bb1e1fa0797354864
stian96 <stiankru@hiof.no>
2023-08-26 15:01:19 +0200

    working on hamburger menu for mobile.

--- app/static/js/fade.js
+++ app/static/js/fade.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".fade-in").classList.add("active");
+    document.querySelector(".header.fade-in").classList.add("active");
 });
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
+
+document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
